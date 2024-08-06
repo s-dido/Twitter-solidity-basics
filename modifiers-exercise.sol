4b28305873e1255144b259646063bb5ca4bcfafd
s-dido <didomenicosantino29@gmail.com>
2024-08-06 13:30:32 -0300

    create modifiers exercise

---
+++ modifiers-exercise.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
+
+contract PausableToken{
+
+    address public owner;
+    bool public paused;
+    mapping (address => uint) public balances;
+
+    constructor(){
+        owner = msg.sender;
+        paused = false;
+        balances[owner] = 1000;
+    }
+
+    modifier onlyOwner (){
+        // implement the modifier to allow only the owner to call the function;
+        _;
+    }
+
+    // implement the modifier to check if the contract is not paused
+
+    function pause () public onlyOwner{
+        paused = true;
+    }
+
+    function unpause() public onlyOwner{
+        paused = false;
+    }
+
+    function transfer (address to, uint amount) public {
+        require (balances[owner] >= amount, "insuficient balance");
+
+        balances[msg.sender] -= amount;
+        balances[to] += amount;
+    }
+}
